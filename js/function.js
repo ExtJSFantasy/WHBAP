576766c0e65ca0dbd2367f019c4ae93ff7b3c209
xiangdeshan <xiangdeshan_120@sina.com>
2018-03-29 15:11:09 +0800

    添加uuid生成方法

--- js/function.js
+++ js/function.js
@@ -1625,3 +1625,10 @@ function windowsNOkReprint(data,callback) {
 		}
 	}
 }
+//用于生成uuid
+function S4() {
+	return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+}
+function guid() {
+	return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
